93cc3abaa4c62c87f356c4b994ac891bd4ac3257
s1rius <s1rius.noone@gmail.com>
2022-07-10 16:21:32 +0800

    separate android preview to a single crate

--- examples/android_preview/src/lib.rs
+++ examples/android_preview/src/lib.rs
@@ -1,14 +1,25 @@
 use ezlog::{
-    init, CipherKind, CompressKind, EZLogConfig, EZLogConfigBuilder, EZRecordBuilder,
+    CipherKind, CompressKind, EZLogConfig, EZLogConfigBuilder, EZRecordBuilder, EventPrinter,
     DEFAULT_LOG_NAME,
 };
 
-// #[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
+static EVENT_LISTENER: EventPrinter = EventPrinter;
+
+/// Quick run example on android device
+/// you can run this example, and see the logcat without open AndroidStudio/IDEA
+/// 
+/// # Example
+/// 
+/// ```shell
+/// cargo install cargo-apk
+/// cargo apk run -p ezlog-android-preview
+/// adb logcat --pid=$(adb shell pidof -s rust.ezlog_android_preview)
+/// ```
+#[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
 #[allow(dead_code)]
 fn main() {
-    init();
+    ezlog::init_with_event(&EVENT_LISTENER);
     let log_config = get_config();
-
     ezlog::create_log(log_config);
     let record = EZRecordBuilder::new().content("12345".to_string()).build();
     ezlog::log(record);
@@ -21,7 +32,7 @@ fn get_config() -> EZLogConfig {
     let nonce = b"unique nonce";
     EZLogConfigBuilder::new()
         .level(ezlog::Level::Trace)
-        .dir_path("data/data/rust.example.android_hello_world/files/ezlog".to_string())
+        .dir_path("data/data/rust.ezlog_android_preview/files/ezlog".to_string())
         .name(DEFAULT_LOG_NAME.to_string())
         .file_suffix(String::from("mmap"))
         .compress(CompressKind::ZLIB)
