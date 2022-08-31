97022405896e1963234682a29dab772153035983
s1rius <s1rius.noone@gmail.com>
2022-08-31 23:56:21 +0800

    add tauri ui

---
+++ ui/src/components/LogList.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+
+let ologs: any[] = []
+
+async function fetchLogs() {
+            await invoke('read_log')
+                .then((logs: any) => {
+                    ologs = logs;
+                })
+                .catch((error: any) => {
+                    console.error('Error fetching logs:', error);
+                });
+        }
+</script>
+
+<template>
+    <div>
+        <h1>Log List</h1>
+        <ul>
+            <li v-for="log in ologs" :key="log">{{ log }}</li>
+        </ul>
+    </div>
+</template>
+  
+  
