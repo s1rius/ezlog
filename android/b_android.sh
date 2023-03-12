eeed63a378f6452f1f535d39404620ce3b25b1cd
s1rius <s1rius.noone@gmail.com>
2023-03-12 19:42:02 +0800

    update ci

--- android/b_android.sh
+++ android/b_android.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cargo ndk -t armeabi-v7a -t arm64-v8a -o lib-ezlog/src/main/jniLibs build -Zbuild-std -p ezlog --release
+cargo ndk -t armeabi-v7a -t arm64-v8a -o lib-ezlog/src/main/jniLibs build -p ezlog --release
 ./gradlew :lib-ezlog:assembleRelease
