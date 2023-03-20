0e2cc67fceb8c89f0cd02356c32471e6ce7c7a82
s1rius <s1rius.noone@gmail.com>
2023-03-20 23:30:22 +0800

    optimize flutter code

--- ezlog_flutter/lib/ezlog_flutter.dart
+++ ezlog_flutter/lib/ezlog_flutter.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names,
+
 import 'dart:typed_data';
 
 import 'ezlog_flutter_platform_interface.dart';
@@ -30,9 +32,9 @@ class EZLogConfig {
   int maxLevel;
   String dirPath;
   int keepDays;
-  int compress = 0;
-  int compressLevel = 0;
-  int cipher = 0;
+  CompressKind compress = CompressKind.NONE;
+  CompressLevel compressLevel = CompressLevel.Default;
+  CipherKind cipher = CipherKind.NONE;
   Uint8List? cipherKey;
   Uint8List? cipherNonce;
   bool enableTrace = false;
@@ -102,9 +104,9 @@ class EZLogger {
         config.maxLevel,
         config.dirPath,
         config.keepDays,
-        config.compress,
-        config.compressLevel,
-        config.cipher,
+        config.compress.value,
+        config.compressLevel.value,
+        config.cipher.value,
         config.cipherKey,
         config.cipherNonce,
         config.rorateHours,
@@ -137,5 +139,75 @@ class EZLogger {
 
   void trim() {
     EZLogFlutterPlatform.instance.trim();
+  }
+}
+
+enum CipherKind {
+  @Deprecated('Use AES128GCMSIV instead')
+  AES128GCM,
+  @Deprecated('Use AES256GCMSIV instead')
+  AES256GCM,
+  AES128GCMSIV,
+  AES256GCMSIV,
+  NONE,
+  UNKNOWN,
+}
+
+extension CipherKindValue on CipherKind {
+  int get value {
+    switch (this) {
+      // ignore: deprecated_member_use_from_same_package
+      case CipherKind.AES128GCM:
+        return 1;
+      // ignore: deprecated_member_use_from_same_package
+      case CipherKind.AES256GCM:
+        return 2;
+      case CipherKind.AES128GCMSIV:
+        return 3;
+      case CipherKind.AES256GCMSIV:
+        return 4;
+      case CipherKind.NONE:
+        return 0;
+      case CipherKind.UNKNOWN:
+        return 0;
+    }
+  }
+}
+
+enum CompressKind {
+  ZLIB,
+  NONE,
+  UNKNOWN,
+}
+
+extension CompressKindValue on CompressKind {
+  int get value {
+    switch (this) {
+      case CompressKind.ZLIB:
+        return 1;
+      case CompressKind.NONE:
+        return 0;
+      case CompressKind.UNKNOWN:
+        return 0xff;
+    }
+  }
+}
+
+enum CompressLevel {
+  Fast,
+  Default,
+  Best,
+}
+
+extension CompressLevelValue on CompressLevel {
+  int get value {
+    switch (this) {
+      case CompressLevel.Fast:
+        return 1;
+      case CompressLevel.Default:
+        return 0;
+      case CompressLevel.Best:
+        return 2;
+    }
   }
 }
