d6801da4c7d78dadef3196170c14e65487184877
s1rius <s1rius.noone@gmail.com>
2023-05-07 02:51:11 +0800

    make `android_logger` optional

--- android/b_android.sh
+++ android/b_android.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cargo ndk -t armeabi-v7a -t arm64-v8a -t x86 -t x86_64 -o lib-ezlog/src/main/jniLibs build -Zbuild-std -p ezlog --release
+cargo ndk -t armeabi-v7a -t arm64-v8a -t x86 -t x86_64 -o lib-ezlog/src/main/jniLibs build -Zbuild-std -p ezlog --features "android_logger" --release
 ./gradlew :lib-ezlog:assembleRelease
