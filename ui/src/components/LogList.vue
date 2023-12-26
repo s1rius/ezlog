692c7a7b6b9dfa89893d6e57eed550064515997b
s1rius <s1rius.noone@gmail.com>
2023-12-26 20:39:38 +0800

    add tailwind

--- ui/src/components/LogList.vue
+++ ui/src/components/LogList.vue
@@ -4,8 +4,8 @@ import { invoke } from "@tauri-apps/api/tauri";
 
 let ologs: any[] = []
 
-async function fetchLogs() {
-            await invoke('read_log')
+async function fetchLogs(path: string) {
+            await invoke('parse_log_file_to_records', {file_path: path})
                 .then((logs: any) => {
                     ologs = logs;
                 })
@@ -13,6 +13,13 @@ async function fetchLogs() {
                     console.error('Error fetching logs:', error);
                 });
         }
+
+async function parse_header_and_extra(path: string) {
+
+    await invoke('parse_header_and_extra', {file_path: path}).then((result: any) => {
+        console.error("sdfasdf", result);
+    }).catch((error: any) => {})
+}
 </script>
 
 <template>
---
+++ ui/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+      }
+    },
+  },
+  plugins: [],
+}
+
