39d18f90a0d8df4648b5ddb70cc26507e9fa0f33
s1rius <s1rius.noone@gmail.com>
2023-12-26 20:39:38 +0800

    add tailwind

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,15 +2,31 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Greet from "./components/Greet.vue";
+import LogList from "./components/LogList.vue";
+
+import { listen } from '@tauri-apps/api/event'
+
+listen('tauri://file-drop', (event) => {
+  if (event.payload && event.payload.length > 0) {
+      const firstFilePath = event.payload[0];
+      console.log('First file dropped:', firstFilePath);
+      // Now you can do something with the first file path
+      // For example, reading the file content or processing the file
+      
+    }
+})
+
+
+
 </script>
 
 <template>
-  <div class="container">
+  <div class="container mx-auto p-8">
     <h1>Welcome to Tauri!</h1>
 
-    <div class="row">
+    <div class=" bg-white justify-center rounded-lg border border-gray-100 ">
       <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
+        <img src="/vite.svg" class="w-12 h-12 pt-2 drop-shadow-md hover:drop-shadow-xl" alt="Vite logo" />
       </a>
       <a href="https://tauri.app" target="_blank">
         <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
